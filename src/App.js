e8c7c88b559b1d89db8929111d7e96b9d6bb5111
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-12-14 22:21:41 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
